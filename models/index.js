4a693eeacb4bf75d8d647bd3cf0f598fc59f944a
임유선 <1991167@hansung.ac.kr>
2022-05-12 22:21:35 +0900

    :sparkles: 댓글 테이블 생성 및 연관관계 설정 (#6)
    - post:comment = 1:N
    - user:comment = 1:N

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
+const Comment = require('./comment');
 const Hashtag = require('./hashtag');
 
 const db = {};
@@ -13,14 +14,17 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
+db.Comment = Comment;
 db.Hashtag = Hashtag;
 
 User.init(sequelize);
 Post.init(sequelize);
+Comment.init(sequelize);
 Hashtag.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
+Comment.associate(db);
 Hashtag.associate(db);
 
 module.exports = db;
