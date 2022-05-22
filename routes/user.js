c371b4b0c4a23a8c58d185ecab214a7f1577ced8
bobaejin <bo_bae_jin@naver.com>
2022-05-23 01:44:37 +0900

    :sparkles: 프로필 이미지 수정 구현 (#4 #5)
    확장자가 jpg가 아니면 오류남 예외처리 필요.. / 처음 가입했을 때 default 이미지 설정해줘야함

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const { isLoggedIn } = require('./middlewares');
 const User = require('../models/user');
 
 const router = express.Router();
+
+try {
+  fs.readdirSync("profileImg");
+} catch (error) {
+  console.error("profileImg 폴더가 없어 profileImg 폴더를 생성합니다.");
+  fs.mkdirSync("profileImg");
+}
+
+const profileImgUpload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, "profileImg/");
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename("profileImg") + ext);
+    },
+  }),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+router.post("/profileImg", isLoggedIn, profileImgUpload.single("profileImg"), (req, res) => {
+  console.log(req.file);
+  res.json({
+    url: `/profileImg/${req.file.filename}`,
+  });
+});
 
 router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   try {
@@ -20,13 +52,15 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.post('/:id/modify', isLoggedIn, async (req, res, next) => {
+const upload2 = multer();
+
+router.post('/:id/modify', isLoggedIn, upload2.none(), async (req, res, next) => {
   let body = req.body
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
     if (user) {
       await user.update({
-        profileImg: body.profileImg,
+        profileImg: body.url,
         nick: body.nickname,
         email: body.email,
       })
